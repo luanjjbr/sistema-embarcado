50b1b0cf7494eeb86d5c2062ae5ba8ca25c53b5e
Luan Italo <italojjbr@gmail.com>
2026-08-31 07:27:47 -0300

    feat(aula2): adiciona leitura interna de VDD via Bandgap e corrigi leitura do ADC
    Signed-off-by: Luan Italo <italojjbr@gmail.com>

--- Aulas/ALUA2/ALUA2.ino
+++ Aulas/ALUA2/ALUA2.ino
@@ -1,24 +1,50 @@
+#include <Arduino.h>
+
+// Função para ler a tensão interna de referência (1.1V Bandgap)
+int16_t i16_readBandGap()
+{
+    // Seleciona Vcc como referência (REFS0) e canal MUX para a Bandgap de 1.1V (MUX3, MUX2, MUX1)
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    
+    // Pequeno tempo para estabilização da tensão de referência interna
+    delay(2);
+    
+    // Inicia a conversão A/D (ADSC = ADC Start Conversion)
+    ADCSRA |= _BV(ADSC);
+    
+    // Aguarda a conclusão da conversão
+    while (bit_is_set(ADCSRA, ADSC));
+    
+    // ADCW lê automaticamente ADCL primeiro e depois ADCH na ordem correta exigida pelo AVR
+    return ADCW; 
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int i16_ADC_1v1BG = i16_readBandGap();
-  Serial.println("ADC_1v1BG: "+String(i16_ADC_1v1BG));
-  int32_t i32_calcTemp = 1023L * 1100L;
-  i32_calcTemp = i32_calcTemp / i16_ADC_1v1BG;
-  Serial.println("ADC_VDD: "+String(i32_calcTemp));
+    // Leitura do valor bruto do ADC para a referência interna de 1.1V
+    int16_t i16_ADC_1v1BG = i16_readBandGap();
+    
+    // Impressão da leitura bruta
+    Serial.print("ADC_1v1BG: ");
+    Serial.println(i16_ADC_1v1BG);
 
-  delay(1000);
-}
-int16_t i16_readBandGap()
-{
-  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2);
-  ADCSRA |= _BV(ADSC);
-  while(bit_is_set(ADCSRA, ADSC));
-  return (ADCH << 8) | ADCL;
+    // Cálculo da tensão de alimentação VDD em mV:
+    // VDD = (1023 * 1100 mV) / Leitura_ADC
+    if (i16_ADC_1v1BG > 0) 
+    {
+        int32_t i32_calcVDD = 1023L * 1100L;
+        i32_calcVDD = i32_calcVDD / i16_ADC_1v1BG;
+        
+        Serial.print("ADC_VDD (mV): ");
+        Serial.println(i32_calcVDD);
+    }
+    else 
+    {
+        Serial.println("Erro na leitura do ADC!");
+    }
 
+    delay(1000);
 }
